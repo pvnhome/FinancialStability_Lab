e93bccbad2f14c62130d34a66fbb04af7ff6fa96
Victor Pyankov <vp@ubuntu.vr>
2019-08-14 13:57:34 +0600

    Добавил отладочный модуль test.curve_grid_search_copy

--- NelsonSiegel/test_runner.py
+++ NelsonSiegel/test_runner.py
@@ -3,7 +3,8 @@ Created on 6 сент. 2018 г.
 
 @author: victor
 '''
-import test.curve_fixed_tau_copy 
+#import test.curve_fixed_tau_copy 
+import test.curve_grid_search_copy
 
 if __name__ == '__main__':
     print('happy end')
